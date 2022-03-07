9d83c712fac1755b12df7c4f08ee12dbb65ee6bd
Raptor Tech <44273999+coldcape@users.noreply.github.com>
2022-03-07 09:13:37 +0100

    Updating files

---
+++ src/com/company/insertionSort.java
@@ -0,0 +1,106 @@
+package com.company;
+
+public class insertionSort {
+
+
+    public static void selectionSort(int A[])
+    {
+        // Utplukksortering av array med heltall
+
+        int n = A.length;
+        int min, tmp;
+
+        for (int i = 0; i < n-1; i++)
+        {
+            // A er riktig sortert t.o.m. indeks i-1
+            // Finner det minste av de usorterte elementene
+            min = i;
+            for (int j = i+1; j < n; j++)
+                if (A[j] < A[min])
+                    min = j;
+            // Swapper det minste elementet og element nummer i
+            tmp = A[min];
+            A[min] = A[i];
+            A[i] = tmp;
+        }
+    }
+
+    public static void insertionSort(int A[])
+    {
+        // Innstikksortering av array med heltall
+
+        int n = A.length;
+        int key;
+
+        for (int i = 1; i < n; i++)
+        {
+            // A er sortert t.o.m. indeks i-1
+            key = A[i];
+            int j = i;
+            // Setter element nummer i pÃ¥ riktig plass
+            // blant de i-1 fÃ¸rste elementene
+            while (j > 0 && A[j-1] > key)
+            {
+                A[j] = A[j-1];
+                j--;
+            }
+            A[j] = key;
+        }
+    }
+
+    public static void bubbleSort(int A[])
+    {
+        // Bubble sort av array med heltall
+        //
+        // Sorterer anderledes enn lÃ¦rebokas bubble sort, ved at
+        // smÃ¥ elementer her flyttes fremover i arrayen, i stedet for
+        // som i lÃ¦reboka Ã¥ flytte store elementer bakover
+
+        int n = A.length;
+        int tmp;
+
+        for (int i = 0; i < n; i++)
+        {
+            // A er riktig sortert t.o.m. indeks i-1
+            // Flytter det minste av de gjenvÃ¦rende elementene
+            // til indeks i ved Ã¥ swappe smÃ¥ elementer fremover
+            for (int j = n-1; j > i; j--)
+                if (A[j] < A[j-1])
+                {
+                    tmp = A[j];
+                    A[j] = A[j-1];
+                    A[j-1] = tmp;
+                }
+        }
+    }
+
+    public static void shellSort(int A[])
+    {
+        // Shell sort av array med heltall
+        // Gap-sekvens: n/2, (n/2)/2.2, (n/2)/(2.2)^2, (n/2)/2.2^3...
+
+        int n = A.length;
+        int gap = n/2;
+
+        while (gap > 0)
+        {
+            for (int i = gap; i < n; i++)
+            {
+                int j = i;
+                int temp = A[i];
+
+                while (j >= gap && A[j - gap] > temp)
+                {
+                    A[j] = A[j - gap];
+                    j = j - gap;
+                }
+                A[j] = temp;
+            }
+            if (gap == 2)
+                gap = 1;
+            else
+                gap *= (5.0 / 11);
+        }
+    }
+
+}
